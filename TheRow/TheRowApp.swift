6de140973682b2a343b8dc73496e7c27eb741e4b
irinadeeva <deeva97@me.com>
2024-12-16 16:36:33 +0300

    Initial Commit

---
+++ TheRow/TheRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRowApp.swift
+//  TheRow
+//
+//  Created by Irina Deeva on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TheRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
